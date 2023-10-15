2fae4691c5a2d3a327a48d91f9b06200c54da431
biddut mia <test@gamil.com>
2023-10-15 09:42:02 -0700

    jjj

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/app/App";
-import store from "./store";
-import { StoreProvider } from "easy-peasy";
-import { BrowserRouter } from "react-router-dom";
+// import App from "./components/app/App";
+// import store from "./store";
+// import { StoreProvider } from "easy-peasy";
+// import { BrowserRouter } from "react-router-dom";
 import NewApp from "./components/NewApp";
 
 // console.log("easy", JSON.parse(localStorage.getItem("[EasyPeasyStore][0]")));
