4fc144ed9008a2ec4779b14b47c952a7082ae414
biddut mia <test@gamil.com>
2023-10-15 09:34:26 -0700

    ddfd

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./components/app/App";
 import store from "./store";
 import { StoreProvider } from "easy-peasy";
 import { BrowserRouter } from "react-router-dom";
+import NewApp from "./components/NewApp";
 
 // console.log("easy", JSON.parse(localStorage.getItem("[EasyPeasyStore][0]")));
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <StoreProvider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StoreProvider>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <StoreProvider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StoreProvider>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(<NewApp />);
